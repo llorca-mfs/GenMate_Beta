b57bc8b1f5d8f94359d8ef21433063d82f382075
gianissimo0829 <gjbm.exe@gmail.com>
2021-08-27 01:07:11 +0800

    small bug fix
    did not overwrite the old one properly

--- app/src/main/java/com/mobdeve/s17/llorca/madrid/genmate_beta/ui/resin/ResinFragment.java
+++ app/src/main/java/com/mobdeve/s17/llorca/madrid/genmate_beta/ui/resin/ResinFragment.java
@@ -1,36 +1,96 @@
 package com.mobdeve.s17.llorca.madrid.genmate_beta.ui.resin;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+
+import com.mobdeve.s17.llorca.madrid.genmate_beta.R;
 import com.mobdeve.s17.llorca.madrid.genmate_beta.databinding.FragmentResinBinding;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
-public class ResinFragment extends Fragment {
+public class ResinFragment extends AppCompatActivity implements View.OnClickListener{
+
     private FragmentResinBinding binding;
+    private ResinTimeAdapter resinTimeAdapter;
+    private ArrayList<ResinTime> resinTimesList = new ArrayList<ResinTime>();
+
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_resin);
+        createNotificationChannel();
+        binding = FragmentResinBinding.inflate(getLayoutInflater());
+        View mainView = binding.getRoot();
+        setContentView(mainView);
 
-        binding = FragmentResinBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-
+        binding.acbCalculateResinTimes.setOnClickListener(this);
+        resinTimeAdapter = new ResinTimeAdapter(getApplicationContext(), resinTimesList);
+        binding.rvForResinTimes.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        binding.rvForResinTimes.setAdapter(resinTimeAdapter);
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    private void calculateResinTimes(){
+        resinTimesList = new ArrayList<ResinTime>();
+        String curAmountResin = binding.etCurResinInput.getText().toString();
+
+        if (curAmountResin.matches("") || Integer.parseInt(curAmountResin) < 0 || Integer.parseInt(curAmountResin)>=160) {
+            Toast.makeText(this, "You did not enter a valid resin Amount", Toast.LENGTH_LONG).show();
+            resinTimeAdapter = new ResinTimeAdapter(getApplicationContext(), resinTimesList);
+            binding.rvForResinTimes.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+            binding.rvForResinTimes.setAdapter(resinTimeAdapter);
+            return;
+        }
+
+        int parsedCurAmountResin = Integer.parseInt(binding.etCurResinInput.getText().toString());
+        int minuteCounter = 0;
+
+        Calendar currentTimeNow = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm a", Locale.getDefault());
+
+        for (int i = parsedCurAmountResin; i <=160; i++){
+            if (i % 20 == 0 && i != 0 && i != parsedCurAmountResin){
+                resinTimesList.add(new ResinTime(sdf.format(currentTimeNow.getTime()), i, minuteCounter));
+
+                resinTimeAdapter = new ResinTimeAdapter(getApplicationContext(), resinTimesList);
+                binding.rvForResinTimes.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                binding.rvForResinTimes.setAdapter(resinTimeAdapter);
+            }
+            currentTimeNow.add(Calendar.MINUTE, 8);
+            minuteCounter += 8;
+        }
+
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onClick(View v) {
+        if (v.getId() == R.id.acbCalculateResinTimes) {
+            calculateResinTimes();
+        }
     }
+
+    private void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "GenMateReminderChannel";
+            String description = "Channel for GenMate Reminder";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel ("notifyUserResin", name, importance);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+
 }
